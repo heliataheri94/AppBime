65e9e949a09ca6c17a4a2044738d58e864c21664
heliataheri94 <helia.taheri94@gmail.com>
2020-08-02 22:41:28 -0700

    Update ExamSlider.js
    درست کردن کلاس ها و مرتب سازی

--- assets/js/ExamSlider.js
+++ assets/js/ExamSlider.js
@@ -3,24 +3,29 @@ var count=0
 $(document).ready(function () {
     count = $('.ExamSlider').children('#question').length;
 
-    $('.ExamSlider').children('#question').eq(ShowQuestionNumber).css('display', 'block');
+    $('.ExamSlider').children('#question').eq(ShowQuestionNumber).css({'display': 'block','right': '0%'});
     console.log(count);
 });
 $("#exam_previous").click(function () {
     if (ShowQuestionNumber > 0) {
+        $('.ExamSlider').children('#question').eq(ShowQuestionNumber).css('display', 'block').animate({right: '100%'});
+
         ShowQuestionNumber--;
-        $('.ExamSlider').children('#question').css('display', 'none');
-        $('.ExamSlider').children('#question').eq(ShowQuestionNumber).fadeIn();
-        $('.ExamSlider').children('#question').eq(ShowQuestionNumber).fadeIn("slow");
-        $('.ExamSlider').children('#question').eq(ShowQuestionNumber).fadeIn(3000);
+
+        $('.ExamSlider').children('#question').css({'display': 'none','right': '-100%'});
+        $('.ExamSlider').children('#question').eq(ShowQuestionNumber).css('display', 'block');
+        $('.ExamSlider').children('#question').eq(ShowQuestionNumber).animate({right: '0%'});
+        // $('.ExamSlider').children('#question').eq(ShowQuestionNumber).fadeIn("slow");
+        // $('.ExamSlider').children('#question').eq(ShowQuestionNumber).fadeIn(3000);
     }
 });
 $("#exam_next").click(function () {
     if (ShowQuestionNumber < count -1) {
+        $('.ExamSlider').children('#question').eq(ShowQuestionNumber).css('display', 'block').animate({right: '-100%'});
+
         ShowQuestionNumber++;
-        $('.ExamSlider').children('#question').css('display', 'none');
-        $('.ExamSlider').children('#question').eq(ShowQuestionNumber).fadeIn();
-        $('.ExamSlider').children('#question').eq(ShowQuestionNumber).fadeIn("slow");
-        $('.ExamSlider').children('#question').eq(ShowQuestionNumber).fadeIn(3000);
+        $('.ExamSlider').children('#question').css({'display': 'none','right': '100%'});
+        $('.ExamSlider').children('#question').eq(ShowQuestionNumber).css('display', 'block');
+        $('.ExamSlider').children('#question').eq(ShowQuestionNumber).animate({right: '0%'});
     }
 });
