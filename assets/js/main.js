578b75efa25a8dfccbd6dc96f517883982e293b9
heliataheri94 <helia.taheri94@gmail.com>
2020-08-04 02:57:21 -0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,5 +3,10 @@ $(document).ready(function(){
     $(".Back-Button").click(function(){
         window.history.back();
     });
-    
+    $('[data-pin]').pin({
+        displayMessage: $('.message'),
+        allowSequential: false,
+        allowRepeat: false,
+        count: 6
+    });
 });
